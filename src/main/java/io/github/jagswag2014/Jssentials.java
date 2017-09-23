78068de0f3875381727d5281a9ae15a82c666872
JagSwag2014 <pacman1225@gmail.com>
2017-09-23 16:26:43 -0500

    Starting over, merging JManage and JConomy with Jssentials

---
+++ src/main/java/io/github/jagswag2014/Jssentials.java
@@ -0,0 +1,76 @@
+/*
+ * This file is part of Jssentials. Jssentials is free software:
+ * you can redistribute it or modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version. Jssentials is distributed in the hope
+ * that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License along
+ * with Jssentials. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package io.github.jagswag2014;
+
+import io.github.jagswag2014.configuration.SettingsManager;
+import org.bstats.bukkit.Metrics;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Jssentials extends JavaPlugin {
+
+    /**
+     * Settings manager instance
+     */
+    private final SettingsManager settings = SettingsManager.getInstance();
+
+    @Override
+    public void onEnable() {
+
+        getLogger().info("Registering commands...");
+        setupCommands();
+
+        getLogger().info("Registering events...");
+        setupEvents();
+
+        getLogger().info("Registering database...");
+        setupDatabase();
+
+        getLogger().info("Registering metrics...");
+        setupMetrics();
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+    /**
+     * Register all commands
+     */
+    private void setupCommands() {
+
+    }
+
+    /**
+     * Register all events
+     */
+    private void setupEvents() {
+        PluginManager pm = getServer().getPluginManager();
+
+    }
+
+    /**
+     * Register database
+     */
+    private void setupDatabase() {
+
+    }
+
+    /**
+     * Register metrics
+     */
+    private void setupMetrics() {
+        /*Metrics metrics = */
+        new Metrics(this);
+    }
+}
--- src/main/java/io/github/jagswag2014/configuration/SettingsManager.java
+++ src/main/java/io/github/jagswag2014/configuration/SettingsManager.java
@@ -1,12 +1,12 @@
 /*
- * This file is part of Jssentials-Spigot. Jssentials-Spigot is free software:
+ * This file is part of Jssentials. Jssentials is free software:
  * you can redistribute it or modify it under the terms of the GNU Lesser General Public License
  * as published by the Free Software Foundation, either version 3 of the License,
- * or (at your option) any later version. Jssentials-Spigot is distributed in the hope
+ * or (at your option) any later version. Jssentials is distributed in the hope
  * that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
  * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
  * You should have received a copy of the GNU Lesser General Public License along
- * with Jssentials-Spigot. If not, see <http://www.gnu.org/licenses/>.
+ * with Jssentials. If not, see <http://www.gnu.org/licenses/>.
  */
 
 package io.github.jagswag2014.configuration;
