02a458ffd4cd72f95d1f66c8b78e6f04bbf07beb
JagSwag2014 <pacman1225@gmail.com>
2017-09-23 19:53:19 -0500

    Add database, event, and command parents

---
+++ src/main/java/io/github/jagswag2014/database/Database.java
@@ -0,0 +1,15 @@
+/*
+ * This file is part of Jssentials-Spigot. Jssentials-Spigot is free software:
+ * you can redistribute it or modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version. Jssentials-Spigot is distributed in the hope
+ * that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License along
+ * with Jssentials-Spigot. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package io.github.jagswag2014.database;
+
+public abstract class Database {
+}
